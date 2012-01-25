74e75d3ebe9fd3c4f718456d09147fc7517d99d8
Armand <armand.rossius@gmail.com>
2012-01-25 11:18:43 +0100

    Ajout de commentaires pour le fichier Utils.h

--- SCOR/src/Utils.h
+++ SCOR/src/Utils.h
@@ -8,12 +8,18 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+/* Definition des structures utilisées pour les échanges entre tâches */
+
+/* Structure définissant la position d'un robot */
+
 struct Robot
 {
 	int pos_x;
 	int pos_y;
 	double angle;
 };
+
+/* Structure définissant la position et la vitesse d'une balle */
 
 struct Balle
 {
@@ -23,6 +29,8 @@ struct Balle
 	double vit_y;
 };
 
+/* Définit les ordres possibles que l'IA peut donner à un robot */
+
 enum Type_Ordre_Robot
 {
 	Do_nothing,
@@ -31,11 +39,18 @@ enum Type_Ordre_Robot
 	Shoot_Right
 };
 
+/* Structure définissant un ordre donnée à un robot */
+
 struct Ordre_Robot
 {
+	/* Type d'ordre */
 	Type_Ordre_Robot type_Ordre_Robot;
+	/* éventuel information complémentaire */
 	Robot robot;
 };
+
+/* Structure définissant les messages échangés dans la boîte aux lettres
+ * Bal_Vid_Robot */
 
 struct Msg_Vid_Robot
 {
@@ -44,7 +59,13 @@ struct Msg_Vid_Robot
 	Balle balle;
 };
 
+/* Structure définissant les messages échangés dans la boîte aux lettres
+ * Bal_Robot_IA */
+
 typedef Msg_Vid_Robot Msg_Robot_IA ;
+
+/* Structure définissant les messages échangés dans la boîte aux lettres
+ * Bal_IA_Ordre */
 
 struct Msg_IA_Ordre
 {
@@ -52,17 +73,25 @@ struct Msg_IA_Ordre
 	Ordre_Robot ordre_robot2;
 };
 
+/* Structure définissant un mouvement à effectuer par un robot */
+
 struct Pas_Robot
 {
 	int pas_gauche;
 	int pas_droite;
 };
 
+/* Structure définissant les messages échangés dans la boîte aux lettres
+ * Bal_Com_Robot */
+
 struct Msg_Com_Robot
 {
 	Pas_Robot robot1;
 	Pas_Robot robot2;
 };
+
+/* Structure définissant les messages échangés dans la boîte aux lettres
+ * Bal_Ordre_Com */
 
 typedef Msg_Com_Robot Msg_Ordre_Com;
 
