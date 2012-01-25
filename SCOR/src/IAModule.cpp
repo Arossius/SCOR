0db4d430f3e574099a42641eb0c9b648cceba6db
Adrien Brochot <adrien.brochot@gmail.com>
2012-01-25 15:21:17 +0100

    ajout de la classe principale de l'IA

---
+++ SCOR/src/IAModule.cpp
@@ -0,0 +1,41 @@
+/*
+ * IAModule.cpp
+ *
+ *  Created on: 25 janv. 2012
+ *      Author: adrien
+ */
+
+#include "IAModule.h"
+
+IAModule::IAModule() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IAModule::~IAModule() {
+	// TODO Auto-generated destructor stub
+}
+
+
+void * IAModule::Launch(void * arg)
+{
+
+}
+
+
+virtual void IAModule::run()
+{
+	for(;;)
+	{
+
+
+
+
+	}
+}
+
+E_BALL_DIRECTION IAModule::DefinirDirectionBalle(Balle balle)
+{
+
+}
+
---
+++ SCOR/src/IAModule.h
@@ -0,0 +1,49 @@
+/*
+ * IAModule.h
+ *
+ *  Created on: 25 janv. 2012
+ *      Author: adrien
+ */
+
+#ifndef IAMODULE_H_
+#define IAMODULE_H_
+
+#include "Utils.h"
+
+
+enum E_DIRECTION_BALLE {
+	COMING,
+	LEAVING,
+	STOPPED
+}
+
+
+
+
+
+
+class IAModule {
+public:
+	IAModule();
+	virtual ~IAModule();
+
+
+	static void * Launch(void * arg);
+
+protected:
+	virtual void run();
+
+	E_DIRECTION_BALLE DefinirDirectionBalle(Balle balle);
+
+
+
+
+private:
+	Msg_IA_Ordre dernierOrdre;
+	Balle balle;
+	Robot robot1;
+	Robot robot2;
+
+};
+
+#endif /* IAMODULE_H_ */
