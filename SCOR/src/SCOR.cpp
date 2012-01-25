09227b310806809e4e7dc29230d041eca0ba7d0c
Armand <armand.rossius@gmail.com>
2012-01-25 11:50:49 +0100

    Exemple de création de boite aux lettres

--- SCOR/src/SCOR.cpp
+++ SCOR/src/SCOR.cpp
@@ -7,10 +7,18 @@
 //============================================================================
 
 #include <iostream>
+#include <fcntl.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+
 #include "Utils.h"
 using namespace std;
 
 int main() {
+	/* http://linux.die.net/man/3/mq_open */
+	mqd_t coucou = mq_open("coucou", O_RDONLY);
+	mq_unlink("coucou");
+	mq_close(coucou);
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
